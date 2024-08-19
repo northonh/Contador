052f4cd3f35bff84df961d571318f27de6815fe2
northonh <nobile.pedro@gmail.com>
2024-08-19 18:06:39 -0300

    feat: Substitui RadioButtons por Spinner para iniciar a variável contador.

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.contador
 
 import android.os.Bundle
-import android.widget.RadioButton
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.contador.databinding.ActivityMainBinding
 
@@ -15,9 +16,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(amb.root)
 
         // Listener no modo Kotlin
-        // Para o dezRb foi usado o valor em hardcode, mas zeroRb foi usado o valor de texto do componente
-        amb.zeroRb.setOnClickListener { view -> contador = (view as RadioButton).text.toString().toInt() }
-        amb.dezRb.setOnClickListener { contador = 10 }
+        amb.inicialSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Assim como o if, o when é uma expression em Kotlin
+                // Valores em hardcode usados para fins didáticos e economia de tempo
+                contador = when (position) {
+                    0 -> 0
+                    1 -> 5
+                    2 -> 10
+                    else -> 0 // Valor padrão, necessário quando when é usado como expression
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Não utilizado
+            }
+        }
 
         // Listener no modo Kotlin
         amb.cliqueBt.setOnClickListener { amb.contadorTv.text = (++contador).toString() }
