2d267a102363d507332652db15e214cc44f991f7
northonh <nobile.pedro@gmail.com>
2024-08-19 17:27:13 -0300

    feat: Adiciona Button que ao ser clicado atualiza valor do TextView.

--- app/src/main/java/com/example/contador/MainActivity.kt
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.contador
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cliqueBt: Button
+    private lateinit var contadorTv: TextView
+    private var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contadorTv = findViewById(R.id.contadorTv)
+        cliqueBt = findViewById(R.id.cliqueBt)
+
+        // Listener no modo Java
+/*        cliqueBt.setOnClickListener(object: OnClickListener{
+            override fun onClick(v: View?) {
+                contadorTv.setText((++contador).toString())
+            }
+        })*/
+
+        // Listener no modo Kotlin
+        cliqueBt.setOnClickListener { contadorTv.text = (++contador).toString() }
     }
 }
